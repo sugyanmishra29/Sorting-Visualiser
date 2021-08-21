2882cd7e8bc55b5b9aa8f3aa0f30579265a36f67
sugyanmishra29 <sugyanmishra.sinu@gmail.com>
2021-08-21 12:58:57 +0530

    some minor changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
-    E-MAIL:-    tummala.kethan@gmail.com
+    DONE BY:-   SUGYAN MISHRA
+    E-MAIL:-    sugyanmishra.sinu@gmail.com
     *****************
 */
 
